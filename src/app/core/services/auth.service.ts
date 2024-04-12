be4d0f3a7b7f4af8a2878681de6ebe3625022a2c
thanhncgem <thanhnc@gemvietnam.com>
2024-04-12 14:38:07 +0700

    remove cookies when logout

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -48,6 +48,16 @@ export class AuthService {
     localStorage.removeItem('email');
     localStorage.removeItem('user_id');
     localStorage.clear();
+    this.deleteAllCookies();
     this.router.navigate(['/login']);
+  }
+  deleteAllCookies() {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
   }
 }
